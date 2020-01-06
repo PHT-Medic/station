b5e47f572d44a20b7cb15c9be6a970ee38e2af03
Lukas Zimmermann <luk.zim91@gmail.com>
2020-01-06 15:59:37 +0100

    Update: requirements.txt

--- scripts/pip_install.sh
+++ scripts/pip_install.sh
@@ -3,7 +3,5 @@ set -eu
 pip install --upgrade --extra-index-url 'https://pypiserver.lukaszimmermann.dev' --no-cache-dir \
 	'apache-airflow[celery,crypto,password]' \
        	pht-trainlib \
-       	'marshmallow==2.19.5' \
 	psycopg2 \
-        'kombu<4.7,>=4.6.7'
 
