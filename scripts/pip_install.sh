d49ff6bf9b17515a82384f59e0b81c8adc826d2c
Lukas Zimmermann <luk.zim91@gmail.com>
2020-01-08 13:07:40 +0100

    Remove cryprography for now

--- scripts/pip_install.sh
+++ scripts/pip_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 pip install --upgrade --extra-index-url 'https://pypiserver.lukaszimmermann.dev' --no-cache-dir \
-	'apache-airflow[celery,crypto,password]' \
-       	pht-trainlib \
+	'apache-airflow[celery,password]' \
+  pht-trainlib \
 	psycopg2 \
 
