dbcdc21e961923b78f33c59e06c1071cd8c26602
Lukas Zimmermann <luk.zim91@gmail.com>
2020-01-19 12:15:05 +0100

    Add: Redis

--- scripts/pip_install.sh
+++ scripts/pip_install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 pip install --upgrade --extra-index-url 'https://pypiserver.lukaszimmermann.dev' --no-cache-dir \
-	'apache-airflow[celery,password]' \
-  pht-trainlib \
-	psycopg2 \
+    'apache-airflow[celery,password,redis,postgres]' \
+    pht-trainlib \
 
