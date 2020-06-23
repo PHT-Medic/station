02604851c25b624cc41f4770a76c9700a077023c
Michael Slupina <michael.slupina@uni-tuebingen.de>
2020-06-23 14:26:51 +0200

    Remove exception handling and print directly to logfile.

--- dags/airflow_DAG_PHT_pull_image.py
+++ dags/airflow_DAG_PHT_pull_image.py
@@ -19,20 +19,16 @@ default_args = {
 dag = airflow.DAG(dag_id='pull_image', default_args=default_args, schedule_interval=None)
 
 def pull_docker_image(**kwargs):
-    try:
-        repository = kwargs['dag_run'].conf['repository']  # 'harbor.pht.medic.uni-tuebingen.de/library/busybox'
-        tag = kwargs['dag_run'].conf['tag']  # 'latest'
-        # Pull the image.
-        client = docker.from_env()
-        client.images.pull(repository=repository, tag=tag)
-        # Make sure the image with the desired tag is there.
-        images = client.images.list()
-        image_tags = sum([i.tags for i in images], [])
-        assert(':'.join([repository, tag]) in image_tags)
-        ret = "Image was successfully pulled."
-    except:
-        ret = "{} failed".format(pull_docker_image.__name__)
-    return ret
+    repository = kwargs['dag_run'].conf['repository']  # 'harbor.pht.medic.uni-tuebingen.de/library/busybox'
+    tag = kwargs['dag_run'].conf['tag']  # 'latest'
+    # Pull the image.
+    client = docker.from_env()
+    client.images.pull(repository=repository, tag=tag)
+    # Make sure the image with the desired tag is there.
+    images = client.images.list()
+    image_tags = sum([i.tags for i in images], [])
+    assert(':'.join([repository, tag]) in image_tags)
+    print("Image was successfully pulled.")
 
 t1 = PythonOperator(
     task_id='pull_docker_image',
