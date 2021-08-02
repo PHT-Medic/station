4e68d867cde0bcf7a9d3ea1a3105eab8073216fc
migraf <Eufnayg1!>
2021-08-02 15:03:25 +0200

    updated docker compose files and airflow cfg

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -4,13 +4,13 @@ set -e
 
 if [ "$1" = 'webserver' ]; then
     airflow db init
-    airflow users create --username admin --firstname FIRST_NAME --lastname LAST_NAME --role Admin --email admin@example.org -p admin
+    echo $AIRFLOW_USER
+    echo $AIRFLOW_PW
+    airflow users create --username $AIRFLOW_USER --firstname FIRST_NAME --lastname LAST_NAME --role Admin --email admin@station.org -p $AIRFLOW_PW
     airflow scheduler &
     exec  airflow webserver
 
 elif [ "$1" = 'worker' ]; then
-    # airflow db init
-    # airflow users create --username admin --firstname FIRST_NAME --lastname LAST_NAME --role Admin --email admin@example.org -p admin
     exec  airflow worker
 
 elif [ "$1" = 'init' ]; then
