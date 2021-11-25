352b5f5da1a4731fc6c7659d8982520614fcbd55
migraf <michael.graf3110@gmail.com>
2021-11-25 10:51:25 +0100

    update functionality

--- install.sh
+++ install.sh
@@ -9,11 +9,14 @@ set +o noglob
 
 usage=$'Please configure your station in the station.yml file before starting the installation
 Use --worker to spawn a worker node.
+Use --update to update the station software.
 '
 item=0
 
 # worker node disabled by default
 worker=$false
+# update the station software
+update=$false
 
 
 while [ $# -gt 0 ]; do
@@ -23,6 +26,8 @@ while [ $# -gt 0 ]; do
             exit 0;;
             --worker)
             worker=true;;
+          --update)
+            update=true;;
             *)
             note "$usage"
             exit 1;;
@@ -33,6 +38,23 @@ done
 
 workdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $workdir
+
+if [ $worker ]; then
+  note "Starting worker node"
+  docker-compose -f docker-compose.yml -f docker-compose.worker.yml up -d
+  exit 0
+fi
+
+if [ $update ]; then
+  h2 "Updating station software"
+  docker-compose -f docker-compose.yml pull
+  h2 "Shutting down station  ..."
+  docker-compose down -v
+  h2 "Restarting station..."
+  docker-compose up -d
+  success $"----The station has been updated successfully.----"
+  exit 0
+fi
 
 h2 "[Step $item]: checking if docker is installed ..."; let item+=1
 check_docker
