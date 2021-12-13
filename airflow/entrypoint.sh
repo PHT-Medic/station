2fa83bbef579fa1c3fcaee9cc418d18e73587dd5
migraf <michael.graf3110@gmail.com>
2021-12-13 10:46:14 +0100

    added docker keyword for github actions

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -13,8 +13,8 @@ elif [ "$1" = 'worker' ]; then
 
 elif [ "$1" = 'init' ]; then
     airflow db init
-    airflow users create --username admin --firstname FIRST_NAME --lastname LAST_NAME --role Admin --email admin@example.org -p admin
-    airflow connections add 'station_db' --conn-type 'postgres' --conn-login 'admin' --conn-password 'admin' --conn-host 'postgres' --conn-port '5432' --conn-schema "pht_station_${STATION_ID}"
+    airflow users create --username $AIRFLOW_USER --firstname FIRST_NAME --lastname LAST_NAME --role Admin --email admin@example.org -p $AIRFLOW_PW
+    # airflow connections add 'station_db' --conn-type 'postgres' --conn-login 'admin' --conn-password 'admin' --conn-host 'postgres' --conn-port '5432' --conn-schema "pht_station_${STATION_ID}"
     # exec  station_airflow worker
 else
     exec "$@"
