3ea40ac76ab524fcc96c1dd484a7cddae63726fc
davidhieber <david.hieber@uni-tuebingen.de>
2023-11-16 13:05:22 +0100

    fix: increace timeout for docker clilent in puch_train_image for larger images

--- airflow/dags/DAG_PHT_run_train.py
+++ airflow/dags/DAG_PHT_run_train.py
@@ -370,7 +370,7 @@ def run_pht_train():
 
     @task()
     def push_train_image(train_state):
-        client = docker.from_env()
+        client = docker.from_env(timeout = 1200)
 
         registry_address = os.getenv("HARBOR_URL").split("//")[-1]
 
